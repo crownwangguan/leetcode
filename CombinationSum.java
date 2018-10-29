b05977f20d4d9b15b9be04a92d34e154ef34aed6
crownwangguan <crown.wg@outlook.com>
2018-10-29 00:35:06 -0400

    combination sum without duplicate, only difference is to check if current index value equal to previous one, if equal means it will be used in next recursive anyway. index add 1 compare to previous combination because its don't want reuse

--- CombinationSum.java
+++ CombinationSum.java
@@ -6,6 +6,7 @@ public class CombinationSum {
 
     public static void main(String[] args) {
         new CombinationSum().combinationSum(new int[] {2,3,6,7}, 7);
+        new CombinationSum().combinationSum2(new int[] {10, 1, 2, 7, 6, 1, 5}, 8);
     }
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
@@ -15,7 +16,7 @@ public class CombinationSum {
         return returnList;
     }
 
-    public void backtrack(List<List<Integer>> returnList, List<Integer> tempList, int[] candidates, int target, int start) {
+    private void backtrack(List<List<Integer>> returnList, List<Integer> tempList, int[] candidates, int target, int start) {
         if (target < 0) {
             return;
         }
@@ -28,6 +29,32 @@ public class CombinationSum {
             tempList.add(candidates[i]);
             backtrack(returnList, tempList, candidates, target - candidates[i], i);
             tempList.remove(tempList.size() - 1);
+        }
+    }
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> returnList = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack2(returnList, new ArrayList<Integer>(), candidates, target, 0);
+        return returnList;
+    }
+    
+    private void backtrack2(List<List<Integer>> returnList, List<Integer> tempList, int[] candidates, int target, int index) {
+        if (target < 0) {
+            return;
+        }
+        if (target == 0) {
+            System.out.println(tempList);
+            returnList.add(new ArrayList<>(tempList));
+            return;
+        }
+        for ( int i = index; i < candidates.length; i++) {
+            if (i > index && candidates[i] == candidates[i-1] ) {
+                continue;
+            }
+            tempList.add(candidates[i]);
+            backtrack2(returnList, tempList, candidates, target-candidates[i], i+1);
+            tempList.remove(tempList.size()-1);
         }
     }
 }
