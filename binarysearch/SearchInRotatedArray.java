bcfb595c2b11a55777169d3a88b5c64793219f25
crownwangguan <crown.wg@outlook.com>
2018-11-06 23:20:08 -0500

    binary search, find if value exists, first step is still check mid and left/right

--- binarysearch/SearchInRotatedArray.java
+++ binarysearch/SearchInRotatedArray.java
@@ -27,8 +27,37 @@ public class SearchInRotatedArray {
         return nums[left] == target ? left : -1;
     }
 
+    public boolean search2(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] < nums[right] || nums[mid] < nums[left]) {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else if (nums[mid] > nums[right] || nums[mid] > nums[left]) {
+                if (nums[mid] > target && nums[left] <= target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                right--;
+            }
+        }
+         return false;
+    }
+
     public static void main(String[] args) {
         int[] nums = new int[]{4,5,6,7,0,1,2};
         System.out.println(new SearchInRotatedArray().search(nums, 5));
+        int[] nums2 = new int[]{2,5,6,0,0,1,2};
+        System.out.println(new SearchInRotatedArray().search2(nums2, 5));
     }
 }
